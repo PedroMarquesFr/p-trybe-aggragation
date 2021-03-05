acca1747ecbfb160bebc773c36c562d7a9cd8dd7
Pedro MF <pedromarques1011@gmail.com>
2021-03-05 12:15:15 -0300

    10,11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,6 @@
 db.trips.aggregate([
   { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
-  { $project: { _id: 0, total: 1, diaDaSemana: "$_id" } },
+  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } },
   { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
